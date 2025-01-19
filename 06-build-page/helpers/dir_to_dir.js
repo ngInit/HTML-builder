006d378353e1c5081e2ab12925f7119fbe4bd884
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-01-19 21:36:03 +0100

    refactor: add modules from tasks 04-copy-directory and 05-merge-styles

---
+++ 06-build-page/helpers/dir_to_dir.js
@@ -0,0 +1,80 @@
+const fileStream = require('node:fs');
+const path = require('node:path');
+
+const originalDirectory = 'files';
+const newDirectory = 'files-copy';
+
+const writeOptions = {
+  flags: 'w',
+};
+
+function copyDir(copyFrom, copyTo) {
+  const dirForCopy = path.join(__dirname, copyFrom);
+  const newDirPath = path.join(__dirname, copyTo);
+  createDirectory(dirForCopy, newDirPath);
+}
+
+function createDirectory(copyFrom, copyTo) {
+  fileStream.mkdir(copyTo, (error) => {
+    const rootName = path.dirname(copyTo);
+    const dirName = path.basename(copyTo);
+    if (error) {
+      console.log(`Directory ${dirName} exists in ${rootName}`);
+    } else {
+      console.log(`Directory ${dirName} created in ${rootName}`);
+    }
+  });
+  collectFiles(copyFrom, copyTo);
+}
+
+function collectFiles(collectFrom, newDirPath) {
+  fileStream.readdir(collectFrom, (error, files) => {
+    if (error || !files.length) {
+      throw error;
+    }
+    files.forEach((file) => {
+      const fileBase = path.parse(file).base;
+      const originalFileLocation = path.join(collectFrom, fileBase);
+      const targetFileLocation = path.join(newDirPath, fileBase);
+      checkIsFile(originalFileLocation, targetFileLocation);
+    });
+  });
+}
+
+function checkIsFile(originalFileLocation, targetFileLocation) {
+  fileStream.stat(originalFileLocation, (error, stats) => {
+    if (error) {
+      console.log('Error:', error.message);
+    }
+    if (!stats.isDirectory()) {
+      copyFile(originalFileLocation, targetFileLocation);
+    } else {
+      createDirectory(originalFileLocation, targetFileLocation);
+    }
+  });
+}
+
+function copyFile(origFilePath, newFilePath) {
+  const readStream = fileStream.createReadStream(origFilePath);
+  const writeStream = fileStream.createWriteStream(newFilePath, writeOptions);
+  readStream.on('data', (data) => {
+    writeStream.write(data);
+    writeStream.close();
+  });
+  writeStream.on('finish', () => {
+    readStream.close();
+  });
+  readStream.on('close', () => {
+    showInfo(origFilePath, newFilePath);
+  });
+}
+
+function showInfo(origFilePath, newFilePath) {
+  let origPath = path.relative(newFilePath, origFilePath);
+  let targetPath = path.relative(origFilePath, newFilePath);
+  origPath = origPath.replaceAll(`..${path.sep}`, '');
+  targetPath = targetPath.replaceAll(`..${path.sep}`, '');
+  console.log(`Copying from ${origPath}  to  ${targetPath} done!`);
+}
+
+copyDir(originalDirectory, newDirectory);
---
+++ 06-build-page/helpers/merge_css.js
@@ -0,0 +1,83 @@
+const fileStream = require('node:fs');
+const path = require('node:path');
+
+const sourceDir = 'styles';
+const targetDir = 'project-dist';
+const targetName = 'bundle';
+
+let mapOfCss = new Map();
+const readOptions = {
+  encoding: 'utf8',
+};
+const createFileOptions = {
+  flags: 'w',
+  encoding: 'utf8',
+};
+const writeOptions = {
+  flags: 'a',
+  encoding: 'utf8',
+};
+
+function buildCSS(sourceCss, targetCss) {
+  const pathWithSources = path.join(__dirname, sourceCss);
+  const pathForResult = path.join(__dirname, targetCss);
+  collectAllCSS(pathWithSources, pathForResult);
+}
+
+function collectAllCSS(sourcesPath, targetPath) {
+  fileStream.readdir(sourcesPath, (error, files) => {
+    if (error || !files.length) {
+      throw error;
+    }
+    files.forEach((file) => {
+      const fileExtension = path.parse(file).ext.slice(1);
+      const fileName = path.parse(file).name;
+      if (fileExtension === 'css') {
+        const fileBase = path.parse(file).base;
+        const locationOfFile = path.join(sourcesPath, fileBase);
+        mapOfCss.set(fileName, locationOfFile);
+      }
+    });
+    createOutputFile(mapOfCss, targetPath);
+  });
+}
+
+function createOutputFile(mapOfCss, targetPath) {
+  const outputFilePath = path.join(targetPath, targetName + '.css');
+  const makeFileStream = fileStream.createWriteStream(
+    outputFilePath,
+    createFileOptions,
+  );
+  makeFileStream.write('', (error) => {
+    if (error) {
+      console.log('Error:', error.message);
+    } else {
+      makeFileStream.close();
+    }
+  });
+  makeFileStream.on('finish', () => {
+    mergeCss(mapOfCss, outputFilePath);
+  });
+}
+
+function mergeCss(mapOfCss, outputFilePath) {
+  mapOfCss.forEach((filePath, key) => {
+    const readFileStream = fileStream.createReadStream(filePath, readOptions);
+    const writeFileStream = fileStream.createWriteStream(
+      outputFilePath,
+      writeOptions,
+    );
+    mapOfCss.delete(key);
+    const endOfLine = mapOfCss.size ? '\n' : '';
+    readFileStream.on('data', (data) => {
+      writeFileStream.write(data + endOfLine);
+      readFileStream.close();
+    });
+    readFileStream.on('close', () => {
+      writeFileStream.close();
+    });
+  });
+  console.log('Done!');
+}
+
+buildCSS(sourceDir, targetDir);
