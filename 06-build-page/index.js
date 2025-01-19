cfabea3113793b93e2cfd90c579d94d15eebd08e
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-01-20 00:49:41 +0100

    refactor: add emitter for sequential execution of functions

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,9 +1,22 @@
 const { buildCSS } = require('./helpers/merge_css.js');
+const EventEmitter = require('node:events');
 const path = require('node:path');
+const emitter = new EventEmitter();
+
 const targetDirectoryName = 'project-dist';
 const cssDir = 'styles';
+
 const rootCssPath = path.join(__dirname, cssDir);
+
 const targetPath = path.join(__dirname, targetDirectoryName);
 const targetCssPath = targetPath;
 const targetCssName = 'style';
+
+emitter.on('dir', () => {
+  emitter.emit('css');
+});
+emitter.on('css', () => {
   buildCSS(rootCssPath, targetCssPath, targetCssName);
+});
+
+emitter.emit('dir');
