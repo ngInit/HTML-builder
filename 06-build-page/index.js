86dc4821ce19fd2d316477464c3000d268d9b035
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-01-20 00:53:20 +0100

    feat: add copyDir function from dir_to_dir.js module

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,18 +1,23 @@
+const { copyDir } = require('./helpers/dir_to_dir.js');
 const { buildCSS } = require('./helpers/merge_css.js');
 const EventEmitter = require('node:events');
 const path = require('node:path');
 const emitter = new EventEmitter();
 
 const targetDirectoryName = 'project-dist';
+const assetsDir = 'assets';
 const cssDir = 'styles';
 
+const rootAssetsPath = path.join(__dirname, assetsDir);
 const rootCssPath = path.join(__dirname, cssDir);
 
 const targetPath = path.join(__dirname, targetDirectoryName);
+const targetAssetsPath = path.join(targetPath, assetsDir);
 const targetCssPath = targetPath;
 const targetCssName = 'style';
 
 emitter.on('dir', () => {
+  copyDir(rootAssetsPath, targetAssetsPath);
   emitter.emit('css');
 });
 emitter.on('css', () => {
