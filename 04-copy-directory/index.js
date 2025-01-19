5902314690a1590cc5e823e64a7b855f9cd44b2e
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-01-19 16:26:06 +0100

    feat: add createDirectory function for checking folder and creating it if it not exists according to 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,6 +7,19 @@ const newDirectory = 'files-copy';
 function makeCopyOfDir(copyFrom, copyTo) {
   const dirForCopy = path.join(__dirname, copyFrom);
   const newDirPath = path.join(__dirname, copyTo);
+  createDirectory(dirForCopy, newDirPath);
+}
+
+function createDirectory(copyFrom, copyTo) {
+  fileStream.mkdir(copyTo, (error) => {
+    const rootName = path.dirname(copyTo);
+    const dirName = path.basename(copyTo);
+    if (error) {
+      console.log(`Directory ${dirName} exists in ${rootName}`);
+    } else {
+      console.log(`Directory ${dirName} created in ${rootName}`);
+    }
+  });
 }
 
 makeCopyOfDir(originalDirectory, newDirectory);
