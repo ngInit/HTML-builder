fbce5f0f114ebd3a06c2d393ed5e9a47cfd898c7
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-01-20 14:22:25 +0100

    refactor: move code for build configuration to own module build_paths.js

---
+++ 06-build-page/helpers/build_paths.js
@@ -0,0 +1,65 @@
+const path = require('node:path');
+
+let targetDirectoryName = '';
+let componentsDir = '';
+let assetsDir = '';
+let cssDir = '';
+let baseHtml = '';
+let targetCssName = '';
+let targetHtml = '';
+
+let rootDir = '';
+let rootComponentsPath = '';
+let rootAssetsPath = '';
+let rootCssPath = '';
+let rootHtmlPath = '';
+let targetPath = '';
+let targetAssetsPath = '';
+let targetCssPath = '';
+
+function setBuildConfig(config) {
+  targetDirectoryName = config.targetDirectoryName;
+  componentsDir = config.componentsDir;
+  assetsDir = config.assetsDir;
+  cssDir = config.cssDir;
+  baseHtml = config.baseHtml;
+  targetCssName = config.targetCssName;
+  targetHtml = config.targetHtml;
+  setPaths();
+}
+
+function setPaths() {
+  rootDir = path.parse(__dirname).dir;
+  rootComponentsPath = path.join(rootDir, componentsDir);
+  rootAssetsPath = path.join(rootDir, assetsDir);
+  rootCssPath = path.join(rootDir, cssDir);
+  rootHtmlPath = path.join(rootDir, baseHtml);
+  targetPath = path.join(rootDir, targetDirectoryName);
+  targetAssetsPath = path.join(targetPath, assetsDir);
+  targetCssPath = targetPath;
+}
+
+function getBuildConfig({ ...config }) {
+  setBuildConfig(config);
+  return {
+    targetDirectoryName: targetDirectoryName,
+    componentsDir: componentsDir,
+    assetsDir: assetsDir,
+    cssDir: cssDir,
+    baseHtml: baseHtml,
+    targetCssName: targetCssName,
+    targetHtml: targetHtml,
+    rootDir: rootDir,
+    rootComponentsPath: rootComponentsPath,
+    rootAssetsPath: rootAssetsPath,
+    rootCssPath: rootCssPath,
+    rootHtmlPath: rootHtmlPath,
+    targetPath: targetPath,
+    targetAssetsPath: targetAssetsPath,
+    targetCssPath: targetCssPath,
+  };
+}
+
+module.exports = {
+  getBuildConfig,
+};
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,20 +1,6 @@
 const { copyDir } = require('./helpers/dir_to_dir.js');
 const { buildCSS } = require('./helpers/merge_css.js');
 const EventEmitter = require('node:events');
-
-const targetDirectoryName = 'project-dist';
-const componentsDir = 'components';
-const assetsDir = 'assets';
-const cssDir = 'styles';
-const baseHtml = 'template.html';
-const targetCssName = 'style';
-
-const rootAssetsPath = path.join(__dirname, assetsDir);
-const rootCssPath = path.join(__dirname, cssDir);
-
-const targetAssetsPath = path.join(targetPath, assetsDir);
-const targetCssPath = targetPath;
-
 const emitter = new EventEmitter();
 
 
