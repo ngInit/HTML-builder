3f5a0c4b0f1d786dc287787a5b9c16821ed9a2c8
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-01-20 14:43:20 +0100

    refactor: add configuration to emitter with buildHtml function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,6 @@
 const { copyDir } = require('./helpers/dir_to_dir.js');
 const { buildCSS } = require('./helpers/merge_css.js');
+const { buildHtml } = require('./helpers/merge_html.js');
 const { getBuildConfig } = require('./helpers/build_paths.js');
 const EventEmitter = require('node:events');
 const emitter = new EventEmitter();
@@ -25,5 +26,7 @@ emitter.on('css', (config) => {
 });
 
 emitter.on('components', (config) => {
+  buildHtml(config);
 });
+
 emitter.emit('start', getBuildConfig(buildConfig));
