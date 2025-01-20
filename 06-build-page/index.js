53b5e48c50bce09e6931c7e2d01eb2a2dd825f04
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-01-20 16:45:18 +0100

    refactor: add comment for check-cross reviewers

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,3 +1,16 @@
+/**/
+/* ====>   ОБРАТИ ВНИМАНИЕ !   <====
+ *
+ * For CROSS-CHECK you have to copy 4 more modules from my repo:
+ * Для CROSS-CHECK надо скопировать ещё 4 модуля из моего репо:
+ *   build_paths.js
+ *   dir_to_dir.js
+ *   merge_css.js
+ *   merge_html.js
+ * In them I reused the code from tasks 04 and 05 for convenience.
+ * В них я переиспользовал код из тасков 04 и 05 для удобства.
+ */
+
 const { copyDir } = require('./dir_to_dir.js');
 const { buildCss } = require('./merge_css.js');
 const { buildHtml } = require('./merge_html.js');
